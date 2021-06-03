429acd50cf4cc38d75ba095a773348ef30ec7506
tfmeads <taylormeads1@gmail.com>
2021-06-03 12:13:45 -0700

    reorganized project structure, added MuseNote component to each open string bubble

--- Assets/Scripts/MuseNote.cs
+++ Assets/Scripts/MuseNote.cs
@@ -10,8 +10,8 @@ public class MuseNote : MonoBehaviour
 
     enum NoteValue { A = 0, Bb = 1, B = 2, C = 3, Db = 4, D = 5, Eb = 6, E = 7, F = 8, Gb = 9, G = 10, Ab = 11};
 
-    public int value;
-    public int octave;
+    private int value;
+    private int octave;
 
     // Start is called before the first frame update
     void Start()
@@ -30,11 +30,12 @@ public class MuseNote : MonoBehaviour
 
     private bool isValidNoteString(string note)
     {
-        if (note.Length < 2)
+        if (note != null && note.Length < 2)
             return false;
 
+        
         String pitch = note.Substring(0, note.Length - 1);
-        String oct = note.Substring(note.Length - 1, note.Length);
+        String oct = note.Substring(note.Length - 1, 1);
 
         Debug.Log(pitch + " / " + oct);
 
