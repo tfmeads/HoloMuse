f186ec17da74c8ab545fe2ab31f7dfa522927408
tfmeads <taylormeads1@gmail.com>
2021-06-03 11:44:45 -0700

    rebased existing assets in MRTK example based app to fix spatial awareness issues

---
+++ Assets/MuseNote.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuseNote : MonoBehaviour
+{
+    [SerializeField]
+    public string note;
+
+    enum NoteValue { A = 0, Bb = 1, B = 2, C = 3, Db = 4, D = 5, Eb = 6, E = 7, F = 8, Gb = 9, G = 10, Ab = 11};
+
+    public int value;
+    public int octave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isValidNoteString(note))
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private bool isValidNoteString(string note)
+    {
+        if (note.Length < 2)
+            return false;
+
+        String pitch = note.Substring(0, note.Length - 1);
+        String oct = note.Substring(note.Length - 1, note.Length);
+
+        Debug.Log(pitch + " / " + oct);
+
+        return true;
+    }
+}
---
+++ Assets/NoteManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject bubbles = GameObject.Find("OpenStringBubbles");
+
+        foreach(Transform child in bubbles.transform){
+            Debug.Log(child.name);
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
