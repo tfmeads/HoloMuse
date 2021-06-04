9b97e8335cdd824fa1c4ac36f4ab60df686d229a
tfmeads <taylormeads1@gmail.com>
2021-06-04 12:50:11 -0700

    added FixDeadInspectors script to fix editor hanging

--- Assets/Scripts/MuseNote.cs
+++ Assets/Scripts/MuseNote.cs
@@ -8,9 +8,9 @@ public class MuseNote : MonoBehaviour
     [SerializeField]
     public string note;
 
-    enum NoteValue { A = 0, Bb = 1, B = 2, C = 3, Db = 4, D = 5, Eb = 6, E = 7, F = 8, Gb = 9, G = 10, Ab = 11};
+    public enum NoteValue { A = 0, Bb = 1, B = 2, C = 3, Db = 4, D = 5, Eb = 6, E = 7, F = 8, Gb = 9, G = 10, Ab = 11};
 
-    private int value;
+    private string pitch;
     private int octave;
 
     // Start is called before the first frame update
@@ -33,12 +33,30 @@ public class MuseNote : MonoBehaviour
         if (note != null && note.Length < 2)
             return false;
 
-        
-        String pitch = note.Substring(0, note.Length - 1);
-        String oct = note.Substring(note.Length - 1, 1);
 
-        Debug.Log(pitch + " / " + oct);
+        try
+        {
+            pitch = note.Substring(0, note.Length - 1);
+
+            String octString = note.Substring(note.Length - 1, 1);
+            octave = System.Int32.Parse(octString);
+        }
+        catch(Exception e)
+        {
+            Debug.Log("MuseNote parsing failed, " + e.Message);
+            return false;
+        }
 
         return true;
+    }
+
+    public NoteValue GetNoteValue()
+    {
+        return (NoteValue) Enum.Parse(typeof(NoteValue), pitch);
+    }
+
+    public override string ToString()
+    {
+        return note;
     }
 }
--- Assets/Scripts/NoteManager.cs
+++ Assets/Scripts/NoteManager.cs
@@ -7,11 +7,19 @@ public class NoteManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //TODO change to find co-sibling in Fretboard prefab for efficiency's sake
+        ///and in case we want to have multiple notemanagers/fretboards in the future
         GameObject bubbles = GameObject.Find("OpenStringBubbles");
 
         foreach(Transform child in bubbles.transform){
             Debug.Log(child.name);
 
+            MuseNote note = child.GetComponent<MuseNote>();
+
+            if(note != null)
+            {
+                Debug.Log(note.GetNoteValue());
+            }
         }
 
     }
