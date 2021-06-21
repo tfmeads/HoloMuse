b11195911216792661453c4e4bc7ef984c7f47c8
tfmeads <taylormeads1@gmail.com>
2021-06-21 14:12:14 -0700

    in-app debug window

---
+++ Assets/Scripts/DebugWindow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DebugWindow : MonoBehaviour
+{
+    public TextMeshPro textMesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    void OnEnable()
+    {
+        Application.logMessageReceived += LogMessage;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= LogMessage;
+    }
+
+    public void LogMessage(string message, string stackTrace, LogType type)
+    {
+        if (textMesh.text.Length > 300)
+        {
+            textMesh.text = message + "\n";
+        }
+        else
+        {
+            textMesh.text += message + "\n";
+        }
+    }
+}
---
+++ Assets/Scripts/KeySelectionHandler.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeySelectionHandler : MonoBehaviour, IMixedRealityPointerHandler
+{
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        Debug.Log(eventData.ToString());
+        Debug.Log(eventData.Pointer.Position.ToString());
+    }
+
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+        
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
