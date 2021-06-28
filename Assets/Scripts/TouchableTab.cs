89ddb2b46ed6526f42b49dd38714e5742f6515c7
tfmeads <taylormeads1@gmail.com>
2021-06-28 11:57:18 -0700

    base implementation of MainMenu and tabs

---
+++ Assets/Scripts/TouchableTab.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchableTab : MonoBehaviour, IMixedRealityPointerHandler
+{
+
+    public string TabTitle;
+
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        Debug.Log(TabTitle + " tab clicked!!!");
+    }
+
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
