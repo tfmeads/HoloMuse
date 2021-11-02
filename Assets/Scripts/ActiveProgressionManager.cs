884aa1e5acc17434ea5360e23f0961a20ad2600a
tfmeads <taylormeads1@gmail.com>
2021-11-02 12:59:48 -0700

    basic metronome in ActiveProgression tab

--- Assets/Scripts/ActiveProgressionManager.cs
+++ Assets/Scripts/ActiveProgressionManager.cs
@@ -16,6 +16,19 @@ public class ActiveProgressionManager : MonoBehaviour
     public GameObject startStopBtn;
 
 
+    //Think of as top number in time signature. Currently hardcoded to 4/4
+    private int beatsPerMeasure = 4;
+
+    //Represents what beat we're currently in. incremented by metronome and reset to 1 every measure
+    private int currentBeat = 1;
+
+    //Beats per minute of metronome
+    public int activeBPM = 60;
+    private readonly int BPM_MINIMUM = 33;
+    private readonly int BPM_MAXIMUM = 333;
+
+    private AudioSource metronome;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,28 +44,74 @@ public class ActiveProgressionManager : MonoBehaviour
     public void HandleStartStopButton()
     {
 
-        if (startStopBtn != null)
-        {
-            isStarted = !isStarted;
-            startStopBtn.GetComponentInChildren<TextMeshPro>().SetText(isStarted ? "Stop" : "Start");
-        }
-        else
+        if (startStopBtn == null)
         {
             Debug.Log("StartStopButton not found");
             return;
         }
 
+        metronome = GetComponent<AudioSource>();
+
         if (!isStarted)
         {
             //Start metronome routine
             Debug.Log("Starting metronome");
+
+            try
+            {
+                InvokeRepeating(nameof(MetronomeTick), 0, GetQuarterNoteIntervalForBPM(activeBPM));
+            }
+            catch(Exception e)
+            {
+                Debug.LogError(e.Message);
+                return;
+            }
         }
         else
         {
             //Stop metronome routine
             Debug.Log("Stopping metronome");
+            CancelInvoke(nameof(MetronomeTick));
         }
-        
+
+        isStarted = !isStarted;
+        startStopBtn.GetComponentInChildren<TextMeshPro>().SetText(isStarted ? "Stop" : "Start");
+    }
+
+    internal void MetronomeTick()
+    {
+        if (currentBeat > beatsPerMeasure)
+            currentBeat = 1;
+
+        if(currentBeat == 1)
+        {
+            metronome.pitch = 2;
+            metronome.volume = 1;
+            metronome.Play();
+        }
+        else
+        {
+            metronome.pitch = 1;
+            metronome.volume = .8f;
+            metronome.Play();
+        }
+
+
+        currentBeat++;
+    }
+
+    //returns float representing number of seconds = 1 quarter note tick at specified BPM
+    internal float GetQuarterNoteIntervalForBPM(int bpm)
+    {
+        if(bpm < BPM_MINIMUM || bpm > BPM_MAXIMUM)
+        {
+            Debug.LogError(bpm + " is out of allowable BPM range.");
+            return -1f;
+        }
+        else
+        {
+            return 60f / (float) bpm;
+        }
     }
 
     internal void AddChord(Modality targetModality)
